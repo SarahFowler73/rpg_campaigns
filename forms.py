301f4b063475ce8651ba69492ae4c937f88f4a77
Sarah Fowler <sarahf321@hotmail.com>
2016-05-24 21:06:48 -0400

    Create user registration form

---
+++ forms.py
@@ -0,0 +1,51 @@
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import (DataRequired, Regexp, ValidationError, Email,
+                                PasswordField, Length, EqualTo)
+
+from models import User
+
+
+def name_exists(form, field):
+    if User.select().where(User.username == field.data).exists():
+        raise ValidationError('User with that username already exists.')
+
+
+def email_exists(form, field):
+    if User.select().where(User.email == field.data).exists():
+        raise ValidationError('That email is already registered.')
+
+
+class RegistrationForm(Form):
+    username = StringField(
+        'Username',
+        validators=[
+            DataRequired(),
+            Regexp(
+                r'^[a-zA-Z0-9_]+$',
+                message=("Username should be one word, letters, "
+                         "numbers, and underscores only.")
+            ),
+            name_exists
+        ]
+    )
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(),
+            email_exists
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=5),
+            EqualTo('password2', message='Passwords must match')
+        ]
+    )
+    password2 = PasswordField(
+        'Confirm Password',
+        validators=[DataRequired()]
+    )
