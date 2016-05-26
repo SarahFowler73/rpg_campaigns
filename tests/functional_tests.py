ab952d3202d6b8fa8fe53ee75dec969a46757443
Sarah Fowler <sarahf321@hotmail.com>
2016-05-26 18:26:45 -0400

    Add basic login view tests

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -42,18 +42,20 @@ class TestRegistration(BaseTests.BaseFunctionalTest):
     def setUp(self):
         self.driver.get(self.base_url + '/register')
 
+    def tearDown(self):
+        for field in self.select('.field input'):
+            if field.get_attribute('type') in ['text', 'password']:
+                field.clear()
+
     def test_register_exists(self):
         self.assertIn(self.driver.title, 'Registration')
 
     def test_register_has_fields(self):
-        self.assertEqual(
-            len(self.select('.field')),
-            5
-        )  # 4 form fields + csrf token field
+        # 4 form fields + csrf token field
+        self.assertEqual(len(self.select('.field')), 5)
 
     def test_register_has_correct_fields(self):
-        fields = [field.find_element_by_tag_name('input')
-            for field in self.select('.field')]
+        fields = self.select('.field input')
 
         self.assertEqual(fields[0].get_attribute('id'), 'csrf_token')
         self.assertEqual(fields[1].get_attribute('id'), 'username')
@@ -65,6 +67,40 @@ class TestRegistration(BaseTests.BaseFunctionalTest):
         self.assertEqual(fields[2].get_attribute('type'), 'text')
         self.assertEqual(fields[3].get_attribute('type'), 'password')
         self.assertEqual(fields[4].get_attribute('type'), 'password')
+
+    def test_validation_fails_when_incorrect(self):
+        pass
+
+    def test_validation_works_when_correct(self):
+        pass
+
+
+class TestLogin(BaseTests.BaseFunctionalTest):
+    def setUp(self):
+        self.driver.get(self.base_url + '/login')
+
+    def tearDown(self):
+        for field in self.select('.field input'):
+            if field.get_attribute('type') in ['text', 'password']:
+                field.clear()
+
+    def test_login_exists(self):
+        self.assertIn(self.driver.title, 'Login')
+
+    def test_login_has_fields(self):
+        # 2 form fields + csrf token field
+        self.assertEqual(len(self.select('.field')), 3)
+
+    def test_login_has_correct_fields(self):
+        fields = [field.find_element_by_tag_name('input')
+            for field in self.select('.field')]
+
+        self.assertEqual(fields[0].get_attribute('id'), 'csrf_token')
+        self.assertEqual(fields[1].get_attribute('id'), 'email')
+        self.assertEqual(fields[2].get_attribute('id'), 'password')
+
+        self.assertEqual(fields[1].get_attribute('type'), 'text')
+        self.assertEqual(fields[2].get_attribute('type'), 'password')
 
 
 if __name__ == '__main__':
