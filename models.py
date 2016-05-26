4c0e504a1b774749bb72e2bc00790c0fdde94d2c
Sarah Fowler <sarahf321@hotmail.com>
2016-05-25 21:51:48 -0400

    Add draft models for user/game/character relations

--- models.py
+++ models.py
@@ -7,6 +7,45 @@ import peewee as models
 
 DATABASE = models.SqliteDatabase('rpg.db')
 
+
+class Character(models.Model):
+    character = models.PrimaryKeyField(unique=True, null=False)
+    user = models.ForeignKeyField(User)
+    creation_date = models.DateTimeField(default=datetime.datetime.now)
+
+
+class Game(models.Model):
+    title = models.CharField(null=False)
+    creator = models.ForeignKeyField('User', related_name='game_creator')
+    creation_date = models.DateTimeField(default=datetime.default.now)
+    last_active_date = models.DateTimeField()
+
+    class Meta:
+        database = DATABASE
+
+
+class GameCharacter(models.Model):
+    character = models.ForeignKeyField(Character, to_field='character')
+    game = models.ForeignKeyField(Game)
+    stat_type = models.CharField(max_length=255)
+    stat_value = models.TextField(default='')
+
+    class Meta:
+        database = DATABASE
+
+
+class GameSession(models.Model):
+    game = models.ForeignKeyField(Game)
+    session_title = models.CharField(default='')
+    session_text = models.TextField(default='')
+    session_date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=datetime.datetime.now)
+    last_updated = models.DateTimeField()
+
+    class Meta:
+        database = DATABASE
+
+
 class User(UserMixin, models.Model):
     username = models.CharField(unique=True)
     email = models.CharField(unique=True)
@@ -28,6 +67,21 @@ class User(UserMixin, models.Model):
             )
         except models.IntegrityError:
             raise ValueError("User already exists")
+
+
+class UserGame(models.Model):
+    user = models.ForeignKeyField(User)
+    game = models.ForeignKeyField(Game)
+    is_owner = models.BooleanField(default=False)
+    status = models.CharField(
+        choices=('PENDING', 'ACTIVE', 'RETIRED', 'REMOVED')
+    )
+    join_date = models.DateTimeField()
+    last_active_date = models.DateTimeField()
+    exp_date = models.DateTimeField()
+
+    class Meta:
+        database = DATABASE
 
 
 def initialize():
