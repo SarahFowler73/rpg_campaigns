25fc9c80378cb0897d09821ae5db629ae566d598
Sarah Fowler <sarahf321@hotmail.com>
2016-05-27 17:46:50 -0400

    Set null=True on nullable fields; create more tables

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from flask.ext.login import UserMixin
 from flask.ext.bcrypt import generate_password_hash
 import peewee as models
 
-
 DATABASE = models.SqliteDatabase('rpg.db')
 
 
@@ -33,7 +32,7 @@ class User(UserMixin, models.Model):
 
 class Character(models.Model):
     character = models.PrimaryKeyField(unique=True, null=False)
-    user = models.ForeignKeyField(User)
+    user = models.ForeignKeyField(User, related_name='characters')
     creation_date = models.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
@@ -42,19 +41,23 @@ class Character(models.Model):
 
 
 class Game(models.Model):
-    title = models.CharField(null=False)
-    creator = models.ForeignKeyField(User, related_name='game_creator')
+    title = models.CharField(max_length=100, null=False)
+    creator = models.ForeignKeyField(User, related_name='created_games')
     creation_date = models.DateTimeField(default=datetime.datetime.now)
-    last_active_date = models.DateTimeField()
+    last_active_date = models.DateTimeField(null=True)
 
     class Meta:
         database = DATABASE
         indexes = ((('title', 'creator'), True),)
+        order_by = ('last_active_date', 'creation_date')
 
 
 class GameCharacter(models.Model):
-    character = models.ForeignKeyField(Character, to_field='character')
-    game = models.ForeignKeyField(Game)
+    character = models.ForeignKeyField(
+        Character,
+        to_field='character',
+        related_name='games')
+    game = models.ForeignKeyField(Game, related_name='characters')
     stat_type = models.CharField(max_length=255)
     stat_value = models.TextField(default='')
 
@@ -64,27 +67,28 @@ class GameCharacter(models.Model):
 
 
 class GameSession(models.Model):
-    game = models.ForeignKeyField(Game)
+    game = models.ForeignKeyField(Game, related_name='sessions')
     session_title = models.CharField(default='')
     session_text = models.TextField(default='')
-    session_date = models.DateTimeField()
+    session_date = models.DateTimeField(null=True)
     entry_date = models.DateTimeField(default=datetime.datetime.now)
-    last_updated = models.DateTimeField()
+    last_updated = models.DateTimeField(null=True)
 
     class Meta:
         database = DATABASE
+        order_by = ('session_date',)
 
 
 class UserGame(models.Model):
-    user = models.ForeignKeyField(User)
-    game = models.ForeignKeyField(Game)
+    user = models.ForeignKeyField(User, related_name='games')
+    game = models.ForeignKeyField(Game, related_name='users')
     is_owner = models.BooleanField(default=False)
     status = models.CharField(
         choices=('PENDING', 'ACTIVE', 'RETIRED', 'REMOVED')
     )
-    join_date = models.DateTimeField()
-    last_active_date = models.DateTimeField()
-    exp_date = models.DateTimeField()
+    join_date = models.DateTimeField(null=True)
+    last_active_date = models.DateTimeField(null=True)
+    exp_date = models.DateTimeField(null=True)
 
     class Meta:
         database = DATABASE
@@ -93,5 +97,7 @@ class UserGame(models.Model):
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User], safe=True)
+    DATABASE.create_tables(
+        [User, Game, Character, GameCharacter],
+        safe=True)
     DATABASE.close()
