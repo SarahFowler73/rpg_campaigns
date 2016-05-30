e59ce053e021654f77d23d71fbe43f15579dcab7
Sarah Fowler <sarahf321@hotmail.com>
2016-05-30 14:47:36 -0400

    wip register view post create user; convert flashed messages from flask to django

--- rpgs/views.py
+++ rpgs/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 from . import forms
 
@@ -7,4 +11,16 @@ def index(request):
 
 def register(request):
     form = forms.RegisterForm()
+    if request.method == 'POST':
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+            messages.add_message(request, messages.SUCCESS,
+                "Registration complete!"
+            )
+            return HttpResponseRedirect(reverse('index'))
     return render(request, 'register.html', {'form': form})
