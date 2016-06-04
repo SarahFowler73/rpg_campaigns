b84641712a03ba85fa83ec1f89a65f5c800fea26
Sarah Fowler <sarahf321@hotmail.com>
2016-06-04 16:48:56 -0400

    Set up reactjs

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+
 console.log("This is main.js")
+
+class App extends React.Component {
+  render () {
+    return <p> Hello World!</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
